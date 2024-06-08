faf9f6a56aa023242ff10321d43c302f85ae0b2d
GUILHERME N SILVA <guilherme.nogueiras@outlook.com>
2024-06-08 00:33:23 -0300

    test module

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,3 @@
-
 module "aws-s3-bucket-module" {
   source = "./modules/aws/s3"
   bucket_name = var.bucket_name
@@ -22,17 +21,17 @@ module "aws-lambda-exam-module" {
 resource "aws_lambda_permission" "apigw_lambda" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = aws-lambda-exam-module.function_name
+  function_name = module.aws-lambda-exam-module.function_name
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "${aws-api-gateway-module.aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
+  source_arn = "${module.aws-api-gateway-module.execution_arn}/*/*/*"
 }
 
 //Integrate with /exam/{id} endpoint  resource exam_id
 resource "aws_api_gateway_integration" "lambda-gateway-integration" {
-  rest_api_id             = aws-api-gateway-module.api.id
-  resource_id             = aws-api-gateway-module.exam_id.id
-  http_method             = aws-api-gateway-module.get_exam.method
+  rest_api_id             = module.aws-api-gateway-module.api_id
+  resource_id             = module.aws-api-gateway-module.exam_id
+  http_method             = module.aws-api-gateway-module.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = module.aws-lambda-exam-module.invoke_arn
