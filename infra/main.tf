18ddc4954e1732cf555cf28c9b31b1d28f52d6bc
GUILHERME N SILVA <guilherme.nogueiras@outlook.com>
2024-06-09 15:25:17 -0300

    integration method HTTP GET

--- infra/main.tf
+++ infra/main.tf
@@ -122,8 +122,8 @@ resource "aws_api_gateway_integration" "lambda-gateway-integration" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.exam_id.id
   http_method             = aws_api_gateway_method.get_exam.http_method
-  integration_http_method = "POST"
-  type                    = "NONE"
+  integration_http_method = "GET"
+  type                    = "HTTP"
   uri                     = aws_lambda_function.lambda-exam.invoke_arn
 
   depends_on = [aws_lambda_function.lambda-exam]
