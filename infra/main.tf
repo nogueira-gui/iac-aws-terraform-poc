41d02c47de457e0c861013ef870f68e24309a553
GUILHERME N SILVA <guilherme.nogueiras@outlook.com>
2024-06-15 16:06:43 -0300

    fix depends on

--- infra/main.tf
+++ infra/main.tf
@@ -125,7 +125,8 @@ resource "aws_api_gateway_integration" "lambda-gateway-integration-exams" {
 
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [
-    aws_api_gateway_integration.lambda-gateway-integration
+    aws_api_gateway_integration.lambda-gateway-integration-exam_id,
+    aws_api_gateway_integration.lambda-gateway-integration-exams
   ]
 
   rest_api_id = aws_api_gateway_rest_api.api.id
