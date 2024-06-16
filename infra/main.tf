7d28441644483e69c1188cea6127f8af5d7f77a9
GUILHERME N SILVA <guilherme.nogueiras@outlook.com>
2024-06-16 13:23:24 -0300

    feat: deployment description update

--- infra/main.tf
+++ infra/main.tf
@@ -159,4 +159,5 @@ resource "aws_api_gateway_deployment" "deployment" {
 
   rest_api_id = aws_api_gateway_rest_api.api.id
   stage_name  = var.env
+  description = "Deployment for ${var.env} at ${timestamp()}"
 }
