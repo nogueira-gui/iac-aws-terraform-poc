81f100757ec763c7581978ac6651acb876bb528f
GUILHERME N SILVA <guilherme.nogueiras@outlook.com>
2024-10-13 07:20:31 -0300

    feat enable public access

--- infra/main.tf
+++ infra/main.tf
@@ -3,21 +3,14 @@ resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
 }
 
-# S3 Bucket Policy to Allow Public Access
-resource "aws_s3_bucket_policy" "bucket_policy" {
+# S3 Bucket Policy
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
   bucket = aws_s3_bucket.bucket.bucket
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = "*",
-        Action = "s3:GetObject",
-        Resource = "${aws_s3_bucket.bucket.arn}/*"
-      }
-    ]
-  })
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 # S3 Bucket Website Configuration
