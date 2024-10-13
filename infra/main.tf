4da105613375bcd5df90dfa36cf42b83e9959ba6
GUILHERME N SILVA <guilherme.nogueiras@outlook.com>
2024-10-12 22:41:06 -0300

    infra: s3 front allow access

--- infra/main.tf
+++ infra/main.tf
@@ -3,6 +3,45 @@ resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
 }
 
+# S3 Bucket Policy to Allow Public Access
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.bucket.bucket
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = "*",
+        Action = "s3:GetObject",
+        Resource = "${aws_s3_bucket.bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+# S3 Bucket Website Configuration
+resource "aws_s3_bucket_website_configuration" "bucket_website" {
+  bucket = aws_s3_bucket.bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+  # routing_rule {
+  #   condition {
+  #     key_prefix_equals = "docs/"
+  #   }
+  #   redirect {
+  #     replace_key_prefix_with = "documents/"
+  #   }
+  # }
+}
+
 #API GATEWAY
 resource "aws_api_gateway_rest_api" "api" {
   name        = "${var.api_gateway_name}"
